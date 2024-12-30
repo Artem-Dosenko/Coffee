df226dab615af27b29c8f27329049b231f855704
artem <artem.dos.25@gmail.com>
2024-12-30 22:26:37 +0200

    Added body to classes CoffeeOrderBoard, Order and Main

--- src/coffee/order/Main.java
+++ src/coffee/order/Main.java
@@ -1,7 +1,23 @@
 package coffee.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("Alen"));
+        orders.add(new Order("Yoda"));
+        orders.add(new Order("Obi-van"));
+        orders.add(new Order("John Snow"));
 
+        CoffeeOrderBoard orderBoard = new CoffeeOrderBoard();
+        for(Order order : orders){
+            orderBoard.add(order);
+        }
+        orderBoard.draw();
+        orderBoard.deliver();
+        orderBoard.deliver(2);
+        orderBoard.draw();
     }
 }
